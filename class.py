41ff0898acde5249208d97b2c13948c7b624d4d5
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-08-22 22:45:04 +0900

    2nd Python

---
+++ class.py
@@ -0,0 +1,9 @@
+n=int(input())
+i=0
+c=0
+while True:
+    c=c+1
+    i=i+c
+    if n<=i:
+        print(c)
+        break;
--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-num = 100
-result = str(num)
-print(result, type(result))
-
+a='일'
+b='이'
+c='삼'
+n=int(input())
+if n==1:
+    print(a)
+if n==2:
+    print(b)
+if n==3:
+    print(c)
