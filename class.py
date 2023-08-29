594e29edcab39959703085b219b2e3dcfbabcefb
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-08-29 22:53:58 +0900

    9th Python

--- class.py
+++ class.py
@@ -1,4 +1,11 @@
-a, m, d, n=map(int, input().split())
-for i in range(1,n):
-    a=a*m+d
-print(a)
+n = int(input())
+a = input().split()
+
+for i in range(n) :
+    a[i] = int(a[i])
+
+min = a[0]
+for i in range(0, n) :
+    if a[i] < min :
+        min = a[i]
+print(min)
