46e889ebe8559ab21da9851d21bc57dcac031cd1
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-08-29 22:55:47 +0900

    9th Python

--- class.py
+++ class.py
@@ -1,11 +1,4 @@
-n = int(input())
-a = input().split()
-
-for i in range(n) :
-    a[i] = int(a[i])
-
-min = a[0]
-for i in range(0, n) :
-    if a[i] < min :
-        min = a[i]
-print(min)
+n = int(input())  
+num = list(map(int, input().split())) 
+for i in range(n - 1, -1, -1):
+    print(num[i], end=' ')
