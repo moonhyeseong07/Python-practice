d53881c5020e60eed7498c0451c1377ea04adcbd
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-09-06 23:11:43 +0900

    9/6

--- python.py
+++ python.py
@@ -1,9 +1,1 @@
-print("입력")
-n=int(input())
-
-sum=0
-for i in range(1,n+1):
-    if i%2==0:
-        sum=sum+i
-
-print(sum)
+print("Hello World")
--- test.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-num=list(map(int,input().split()))
-result=list(0 for _ in range(24))
-
-for i in range(n):
-    result[num[i]]+=1
-
-for i in range(1,24):
-    print(result[i],end=' ')
