9f556cf0b201121d9e64dc0216c2eb5470dc30c5
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-09-11 17:14:34 +0900

    9/11 python class

--- class.py
+++ class.py
@@ -1,4 +1,9 @@
-n = int(input())  
-num = list(map(int, input().split())) 
-for i in range(n - 1, -1, -1):
-    print(num[i], end=' ')
+arr=[[0 for j in range(19)]for i in range(19)]
+n=int(input())
+for i in range(n):
+    x,y=map(int,input().split())
+    arr[x-1][y-1]=1
+for i in range(19):
+    for j in range(19):
+        print(arr[i][j],end=' ')
+    print()
