995638c3a9f137ac000f493f6e9faafa63a4b7a7
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-10-20 23:09:51 +0900

    10/20

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-def recursive_sum(numbers):
-    if not numbers:
-        return 0
-    return numbers.pop() + recursive_sum(numbers)
-
-def main():
-    numbers = list(map(int, input().split()))
-    result = recursive_sum(numbers)
-    print(result)
-
-if __name__ == "__main__":
-    main()
+n=float(input())
+print(n)
