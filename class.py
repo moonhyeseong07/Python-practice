cb1d7d576914d486239a8bc231a3a074bfe110f2
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-10-30 22:29:28 +0900

    10/30

--- class.py
+++ class.py
@@ -1,8 +1,45 @@
-def countdown_num(n):
-    if n <= 0:
-        return 0
+stack_size=5
+stack=[None]*stack_size
+top=-1
+
+def isEmpty():
+    if top==-1: return True
+    else: return False
+
+def isFull():
+    return top==stack_size-1
+
+def push(item):
+    global top
+    if not isFull():
+        top=top+1
+        stack[top]=item
+        print(stack)
     else:
-        print(n, '이(가) 양수인 경우!')
-        result = countdown_num(n-1) + 1
-        return result
-print('반환값:',countdown_num(100))
+        print("stack is Full")
+
+def pop():
+    global top
+    if not isEmpty():
+        item=stack[top]
+        top=top-1
+        return item
+    else:
+        print("stack is Empty")
+        return None
+    
+def peek():
+    if not isEmpty():
+        return stack[top]
+    else:
+        print("stack is Empty")
+        return None
+push(1)
+push(2)
+push(3)
+push(4)
+push(5)
+pop()
+push(6)
+pop()
+push(7)
