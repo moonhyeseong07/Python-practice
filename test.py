c5d405dda0a5ae5c07686032a6c8a765823d5cc4
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-10-31 22:57:35 +0900

    10/31

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-a,b,c=map(int,input().split())
-list=[a,b,c]
-list.sort()
-print(*list)
+a=int(input())
+while str(a)[-1] == '0':
+    a//= 10
+print(str(a)[::-1])
+b=list(str(a))
+for i in range(len(b)):
+    b[i] = int(b[i])
+print(sum(b))
