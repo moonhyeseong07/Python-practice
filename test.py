af3cf2dd8b4e105e1258302d6d6347066cb69c66
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-11-02 23:02:03 +0900

    11/02

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-a,b,c=map(int,input().split())
-if a%2==0 :
-    print("even")
-else :
-    print("odd") 
-if b%2==0 :
-    print("even")
-else :
-    print("odd") 
-if c%2==0 :
-    print("even")
-else :
-    print("odd") 
+a,b=map(int,input().split())
+print(a%b)
