85b7cbb7cba255a18023202d8c2f3dc5e3d677ad
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-11-05 22:53:18 +0900

    11/05

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-a,b=map(int,input().split())
-def gcd(a,b):
-    for i in range(min(a,b),0,-1):
-        if a%i==0 and b%i==0:
-            return i
-res=gcd(a,b)
-print(res)
+k=int(input())
+for i in range(1,7):
+    for j in range(1,7):
+        if i+j==k:
+            print(i,j)
