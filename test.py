ecdb0567e2b57d675bbc9ef967700de07a7d4fb3
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-11-13 22:45:24 +0900

    11/13

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
-def gcd(x,y):
-    if y==0:
-        return x
-    else:
-        return gcd(y, x % y)
-def gcdthree(a, b, c):
-    return gcd(gcd(a,b),c)
-a,b,c=map(int, input().split())
-result = gcdthree(a, b, c)
-print(result)
+n=int(input())
+for i in range(1,n+1):
+    if i%3==0:
+        continue
+    print(i,end=' ')
