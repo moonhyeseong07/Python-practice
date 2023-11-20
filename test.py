a438641a37defb7c6462de49f4d308e240754b63
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-11-20 23:00:49 +0900

    11/20

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-a,b=map(int,input().split())
-if a>b:
-    print('>')
-elif a<b:
-    print('<')
+t,s=map(int,input().split())
+lt=90-t
+g=0
+if lt%5==0:
+    g=lt//5
 else:
-    print('=')
+    g=lt//5+1
+print(s+g)
