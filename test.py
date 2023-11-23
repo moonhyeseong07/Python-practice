7c871a4791f4c7cfa1e17319f95afaa064b404f7
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-11-23 23:03:24 +0900

    11/23

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-a, b = map(int, input().split())
-print((("홀수" if a%2==1 else "짝수") + "+" + ("홀수" if b%2==1 else "짝수") + "=" + ("짝수" if (a+b)%2==0 else "홀수")))
+n = list(map(int, input().split()))
+n.sort()
+print(n[1])
