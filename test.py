66d2eb42fd75ae87591bb8908926277592438096
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-11-25 22:37:39 +0900

    11/25

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-b,g=input().split()
-g=int(g)
-y=int(b[:2])
-if g==1 or g==2:
-    y+=1900
-else:
-    y+=2000
-age=2012-y+1
-print(age)
+g,c,n=map=input().split()
+n=int(n)
+if n<10:
+    n='0'+str(n)
+print(g+c+n)
