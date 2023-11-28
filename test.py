2b95ba29dcf7959eae04aa3b9e07646fcddc9fba
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-11-28 22:57:13 +0900

    11/28

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-n = list(map(int, input().split()))
-n.sort()
-print(str(n[0])+' '+str(n[1])+' '+str(n[2]))
+h,m=map(int,input().split())
+r=m-30
+if r>=0:
+    print(h,r)
+else:
+    if h==0:
+        print(23,60+r)
+    else:
+        print(h-1,60+r)
