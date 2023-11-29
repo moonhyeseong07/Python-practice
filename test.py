d5f75f3f75720c2a9d743f14e4cebffad7250291
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-11-29 22:54:21 +0900

    11/29

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-h,m=map(int,input().split())
-r=m-30
-if r>=0:
-    print(h,r)
+n=int(input())
+o_num = ''
+if(n%10==1):
+    o_num=str(n)+'st'
+    if(n== 11):
+        o_num=str(n)+'th'
+elif(n % 10 == 2):
+    o_num=str(n)+'nd'
+    if(n==12):
+        o_num=str(n)+'th'
+elif(n%10==3):
+    o_num=str(n)+'rd'
+    if(n==13):
+        o_num=str(n)+'th'
 else:
-    if h==0:
-        print(23,60+r)
-    else:
-        print(h-1,60+r)
+    o_num=str(n)+'th'
+print(o_num)
