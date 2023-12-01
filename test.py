187604491f6a505fde1f4952d2497f272b85a7d7
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-12-01 20:07:35 +0900

    12/1

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-n=int(input())
-if n>0:
-    print("양수")
-elif n<0:
-    print("음수")
-if n==0:
-    print("0")
+score=int(input())
+if score>=90:
+    print("A")
+elif score>=80:
+    print("B")
+elif score>=70:
+    print("C")
+elif score>=60:
+    print("D")
+else:
+    print("F")
