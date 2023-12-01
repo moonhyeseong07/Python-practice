41a411b0032eeeedc8a72134f243f522ca56cbfa
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-12-01 19:58:07 +0900

    12/1

--- test.py
+++ test.py
@@ -1,17 +1,7 @@
 n=int(input())
-o_num = ''
-if(n%10==1):
-    o_num=str(n)+'st'
-    if(n== 11):
-        o_num=str(n)+'th'
-elif(n % 10 == 2):
-    o_num=str(n)+'nd'
-    if(n==12):
-        o_num=str(n)+'th'
-elif(n%10==3):
-    o_num=str(n)+'rd'
-    if(n==13):
-        o_num=str(n)+'th'
-else:
-    o_num=str(n)+'th'
-print(o_num)
+if n>0:
+    print("양수")
+elif n<0:
+    print("음수")
+if n==0:
+    print("0")
