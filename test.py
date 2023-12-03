a41aa9d7a1e7b77e0afc9cc244a21ab960726337
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-12-03 22:49:05 +0900

    12/3

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-menu=[0,400,340,170,100,70]
-menu1, menu2=map(int,input().split())
-c=menu[menu1]+menu[menu2]
-if(c>500):
-    print('angry')
-else:
-    print('no angry')
+n=int(input())
+for i in range(1,n+1):
+    print(i,end=' ')
