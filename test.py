5219c1d97ca63841311b7a9026b31dabbccb104d
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-12-05 23:13:08 +0900

    12/5

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-a,b=map(int,input().split())
-if(a>b):
-    for i in range(b,a+1):
-        print(i,end=' ')
-else:
-    for i in range(a,b+1):
-        print(i,end=' ')
+for i in range(1,101):
+    print(i,end=' ')
