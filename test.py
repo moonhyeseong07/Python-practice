5474a02ead651bcc1bf13786040a141fc3009dbe
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-12-10 11:59:34 +0900

    12/10

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-a,b=map(int,input().split())
-for i in range(a,b+1):
-    if i%2!=0:
-        print(i,end=' ')
+n=int(input())
+sum=0
+for i in range(1,n+1):
+    if i%2==0:
+        sum+=i
+print(sum)
