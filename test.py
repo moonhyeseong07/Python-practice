051c8d5dd9c69eca2325369948521c960740ae28
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-12-13 22:52:05 +0900

    12/13

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-n=int(input())
-for i in range(1,10):
-    print(str(n)+'*'+str(i)+'='+str(n*i))
+a,b,c,d=map(int,input().split())
+bit=a*b*c*d
+byte=bit/8
+kb=byte/1024
+mb=kb/1024
+print("%0.1f MB"%mb)
