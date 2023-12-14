e8027ef6e6b19f00c8c4423817ca47a431b711cb
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-12-14 23:31:45 +0900

    11/31

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-a,b,c,d=map(int,input().split())
-bit=a*b*c*d
-byte=bit/8
-kb=byte/1024
-mb=kb/1024
-print("%0.1f MB"%mb)
+n=int(input())
+num=list(map(int,input().split()))
+sum=0
+for number in num:
+    sum+=number
+print(sum)
