c5637b1507490d268306f91291054b79ec55fd61
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-12-25 19:48:59 +0900

    12/25

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-a,b=map(int,input().split())
-print(a>>b)
+a,b,c=map(int,input().split())
+min=0
+if a<=b and a<=c:
+    min=a
+elif b<=a and b<=c:
+    min=b
+elif c<=a and c<=b:
+    min=c
+print(min)
