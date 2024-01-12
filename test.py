e43afd645ee9df717825ee114cd28f785644a7e3
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-01-13 08:07:12 +0900

    bfs py

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-def dfs(graph, v, visited):
-    # 현재 노드 방문 처리
-    visited[v] = True
-    print(v, end=' ')
+from collections import deque
 
-    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+def bfs(graph, start, visited):
+    queue = deque([start])
+    # node visited
+    visited[start] = True
+
+    # loop till queue is empty
+    while queue:
+        # visted node 'v' 
+        v = queue.popleft()
+        print(v, end=' ')
+
+        # v 노드에 연결된 노드들 투입 -> 방문 안한 노드만 queue에 추가
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                # print(queue) # queue안에 뭐있나
+                visited[i] = True
