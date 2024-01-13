14bf3c8ac88cb60c00b5add8b1bb45aae1d2d26a
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-01-14 08:41:51 +0900

    selection sort py

--- test.py
+++ test.py
@@ -1,19 +1,6 @@
-from collections import deque
-
-def bfs(graph, start, visited):
-    queue = deque([start])
-    # node visited
-    visited[start] = True
-
-    # loop till queue is empty
-    while queue:
-        # visted node 'v' 
-        v = queue.popleft()
-        print(v, end=' ')
-
-        # v 노드에 연결된 노드들 투입 -> 방문 안한 노드만 queue에 추가
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                # print(queue) # queue안에 뭐있나
-                visited[i] = True
+array = [2, 3, 1, 4]
+for i in range(len(array)):
+    min_index = i # index of the smallest element
+    for j in range(i+1, len(array)):
+        min_index = j
+    array[i], array[min_index] = array[min_index], array[i] # swap
