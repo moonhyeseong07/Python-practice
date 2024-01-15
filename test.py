ae30d85a3c5efa97657a01cde0ba05e2da851ec4
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-01-15 22:37:07 +0900

    quick_sort py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 array = [2, 3, 1, 4]
-for i in range(len(array)):
-    min_index = i # index of the smallest element
-    for j in range(i+1, len(array)):
-        min_index = j
-    array[i], array[min_index] = array[min_index], array[i] # swap
+def quick_sort(array):
+    #quit if list has one or less elements
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[0] # first element as pivot
+    tail = array[1:] # list accept pivot
+    left = [x for x in tail if x <= pivot] # left side
+    right = [x for x in tail if x > pivot] # right side
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
