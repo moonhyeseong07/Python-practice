625cd71c46155642930e52edbc42952ce6c5593d
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-01-16 14:03:16 +0900

    binary search py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-array = [2, 3, 1, 4]
-def quick_sort(array):
-    #quit if list has one or less elements
-    if len(array) <= 1:
-        return array
-    
-    pivot = array[0] # first element as pivot
-    tail = array[1:] # list accept pivot
-    left = [x for x in tail if x <= pivot] # left side
-    right = [x for x in tail if x > pivot] # right side
-    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid -1
+        else:
+            start = mid +1
+    return None
+result = binary_search(array, target, 0, n - 1)
