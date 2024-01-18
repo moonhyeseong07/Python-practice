8b9dce9ae0468d002f38714d628afc7daca0705a
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-01-18 21:46:08 +0900

    codeup 1405

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-# Memoization 초기화
-d = [0] * 100
-
-def fibo(x):
-    # base case
-    if x == 1 or x == 2:
-        return 1
-    if d[x] != 0:
-        return d[x]
-    # 점화식을 그대로 구현
-    d[x] = fibo(x-1) + fibo(x-2)
-    return d[x]
+n=int(input())
+a=list(map(int,input().split()))
+for i in range(n):
+    for j in range(n):
+        print(a[i+j-n],end=' ')
+    print()
