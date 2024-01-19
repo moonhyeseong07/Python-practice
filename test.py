e462650ff40ce0317f9ff84ce42737f5740caece
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-01-19 20:32:01 +0900

    codeup 1180

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-n=int(input())
-a=list(map(int,input().split()))
-for i in range(n):
-    for j in range(n):
-        print(a[i+j-n],end=' ')
-    print()
+limit=input()
+m=limit[1]+limit[0]
+m=int(m)*2
+if(m>99):
+    m=m%100
+if(m<=50):
+    print(m)
+    print('GOOD')
+else:
+    print(m)
+    print('OH MY GOD')
