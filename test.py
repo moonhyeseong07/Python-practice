81603e71c5f87d39acbb3556ed4c98c1188dc495
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-01-20 23:22:41 +0900

    codeup 1953

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-limit=input()
-m=limit[1]+limit[0]
-m=int(m)*2
-if(m>99):
-    m=m%100
-if(m<=50):
-    print(m)
-    print('GOOD')
-else:
-    print(m)
-    print('OH MY GOD')
+def f(n,c=1):
+    if c>n:
+        return
+    print("*"*c)
+    f(n,c+1)
+n=int(input())
+f(n)
