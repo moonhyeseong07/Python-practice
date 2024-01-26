d46f7173358bcd9ff794da975da5f29e8e9656c6
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-01-26 13:50:31 +0900

    baekjoon 9498

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print('강한친구 대한육군\n강한친구 대한육군')
+score=int(input())
+if score>=90:
+    print('A')
+elif score>=80 and 89>=score:
+    print('B')
+elif score>=70 and 79>=score:
+    print('C')
+elif score>=60 and 69>=score:
+    print('D')
+else:
+    print('F')
