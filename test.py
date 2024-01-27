a4ce81e29fb2b72c06d0cc9ebd3eadae6f71cc6e
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-01-27 21:53:23 +0900

    baekjoon 2884

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-score=int(input())
-if score>=90:
-    print('A')
-elif score>=80 and 89>=score:
-    print('B')
-elif score>=70 and 79>=score:
-    print('C')
-elif score>=60 and 69>=score:
-    print('D')
-else:
-    print('F')
+h,m=map(int,input().split())
+m-=45
+if m<0:
+    h-=1
+    m+=60
+    if h<0:
+        h=23
+print(h,m)
