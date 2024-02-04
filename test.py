a3fbff01db13a64330b075393b677d94dc508836
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-04 22:57:10 +0900

    baekjoon 2739

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-year=int(input())
-if(year%4==0 and year%100!=0) or (year%400==0):
-    print("1")
-else:
-    print("0")
+n=int(input())
+for i in range(1,10):
+    print(str(n)+" * "+str(i)+" = "+str(n*i))
