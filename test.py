0ac96fb40419879ac562499725615675ec41b5a6
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-09 21:28:23 +0900

    baekjoon 10807

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-n=int(input())
-numbers=list(map(int,input().split()))
-find_num=int(input())
-count=0
-for num in numbers:
-    if num==find_num:
-        count+=1
+n = int(input())
+numbers = list(map(int, input().split()))
+find_num = int(input())
+
+count = numbers.count(find_num)
+
 print(count)
