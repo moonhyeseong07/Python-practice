8497fe76f6718114bf09b1fb2e8555192fd4d98b
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-12 16:07:17 +0900

    baekjoon 10430

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-while True:
-    try:
-        a,b=map(int,input().split())
-        print(a+b)
-    except:
-        break
+a,b,c=map(int,input().split())
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
