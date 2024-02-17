33bffbfe10601bc5e59aa679d109982346686fc1
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-17 22:26:54 +0900

    baekjoon 4101

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-n=int(input())
-result=1
-if n>0:
-    for i in range(1,n+1):
-        result*=i
-print(result)
+while True:
+    a,b=map(int,input().split())
+    if a==0 and b==0:
+        break
+    if a>b:
+        print("Yes")
+    else:
+        print("No")
