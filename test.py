e781c7d52cc21b8ef0f499be46ae9bfdcfc72bf2
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-18 22:01:14 +0900

    baekjoon 1152

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
-while True:
-    a,b=map(int,input().split())
-    if a==0 and b==0:
-        break
-    if a>b:
-        print("Yes")
-    else:
-        print("No")
+w=input().split()
+print(len(w))
