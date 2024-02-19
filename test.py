618a89add9f4263dd302e1ac3c8a0ffa32afcaed
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-19 21:09:28 +0900

    baekjoon 14681

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-w=input().split()
-print(len(w))
+x=int(input())
+y=int(input())
+if x>0:
+    if y>0:
+        print("1")
+    else:
+        print("4")
+else:
+    if x<0:
+        if y<0:
+            print("3")
+        else:
+            print("2")
