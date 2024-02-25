12d4d15ad3021250d07aa4e89b35eab3eb3a567b
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-25 22:40:51 +0900

    baekjoon 10871

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-n=int(input())
-c_o=0
-for i in range(1,n+1):
-    c_o+=str(i)[-1].count('1')
-print(c_o)
+x,y=map(int,input().split())
+n=list(map(int,input().split()))
+for num in n:
+    if num<y:
+        print(num,end=' ')
