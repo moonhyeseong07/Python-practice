7f7af60b95c64a9267826e99f00706d41d25c98c
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-27 21:43:22 +0900

    codeup 1286

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-year=int(input())
-print(year-543)
+numbers=[]
+for i in range(1,6):
+    n=int(input())
+    numbers.append(n)
+max_val=max(numbers)
+min_val=min(numbers)
+print(max_val)
+print(min_val)
