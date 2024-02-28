470d9d80ac6f380856d1914fe37578610df950fe
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-28 21:49:05 +0900

    codeup 1207

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-numbers=[]
-for i in range(1,6):
-    n=int(input())
-    numbers.append(n)
-max_val=max(numbers)
-min_val=min(numbers)
-print(max_val)
-print(min_val)
+a,b,c,d=map(int,input().split())
+result=a+b+c+d
+if result==1:
+    print("도")
+elif result==2:
+    print("개")
+elif result==3:
+    print("걸")
+elif result==4:
+    print("윷")
+else:
+    print("모")
