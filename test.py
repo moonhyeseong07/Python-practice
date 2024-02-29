f5c2c2c89f3dc6ecea64ba8f36d5eb560ffb49a9
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-02-29 20:22:18 +0900

    codeup 1224

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
+def compare(a,b,c,d):
+    fraction1=a/b
+    fraction2=c/d
+
+    if fraction1>fraction2:
+        print(">")
+    elif fraction1==fraction2:
+        print("=")
+    else:
+        print("<")
+
 a,b,c,d=map(int,input().split())
-result=a+b+c+d
-if result==1:
-    print("도")
-elif result==2:
-    print("개")
-elif result==3:
-    print("걸")
-elif result==4:
-    print("윷")
-else:
-    print("모")
+compare(a,b,c,d)
