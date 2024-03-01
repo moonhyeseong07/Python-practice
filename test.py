1cf5b1162286c1d96ed4191dd9efd9fabe6bcfef
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-01 20:37:02 +0900

    codeup 1287

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-def compare(a,b,c,d):
-    fraction1=a/b
-    fraction2=c/d
-
-    if fraction1>fraction2:
-        print(">")
-    elif fraction1==fraction2:
-        print("=")
-    else:
-        print("<")
-
-a,b,c,d=map(int,input().split())
-compare(a,b,c,d)
+n=int(input())
+googoo=0
+for i in range(1,10):
+    googoo+=n
+    print("*"*googoo)
