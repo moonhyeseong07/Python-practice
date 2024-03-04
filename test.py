1bfc8725238cc172fc4f742c4a9d06c9eab15437
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-04 20:29:52 +0900

    baekjoon 2752

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-n=int(input())
-for i in range(1,n+1):
-    space=' '*(n-i)
-    star='*'*i
-    print(space+star)
+a=sorted(list(map(int,input().split())))
+for i in range(3):
+    print(a[i],end=' ')
