6710ce1ea89dd06ae3e07021b114feacee7e08d9
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-09 23:21:59 +0900

    baekjoon 2441

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-while True:
-    name,age,kg=input().split()
-    if '#' in name:
-        break
-    if int(age)>17 or int(kg)>=80:
-        print(f"{name} Senior")
-    else:
-        print(f"{name} Junior")
+n=int(input())
+for i in range(n,0,-1):
+    print(" "*(n-i)+"*"*i)
