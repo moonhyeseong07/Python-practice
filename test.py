8968bf37aa533b8d4ebbf7cbb6f35c8028c4759b
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-10 23:31:14 +0900

    baekjoon 10039

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-n=int(input())
-for i in range(n,0,-1):
-    print(" "*(n-i)+"*"*i)
+sum=0
+for _ in range(5):
+    score=int(input())
+    if score<40:
+        score=40
+    sum+=score
+print(sum//5)
