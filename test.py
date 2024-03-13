5d3f46e75543ff051c5a6f4b7e8f5cb874a01074
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-13 23:11:36 +0900

    baekjoon 1267

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-x,y,w,h=map(int,input().split())
-min_x_distance=min(x,w-x)
-min_y_distance=min(y,h-y)
-result=min(min_x_distance,min_y_distance)
-print(result)
+n=int(input())
+c=list(map(int,input().split()))
+y=0
+m=0
+for i in c:
+    y+=(i//30+1)*10
+    m+=(i//60+1)*15
+if y>m:
+    print("M",m)
+elif y==m:
+    print("Y M",m)
+else:
+    print("Y",y)
