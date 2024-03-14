0a0727580551beec6629c4504b54c037c7c2a32d
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-14 23:34:59 +0900

    baekjoon 3046

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-n=int(input())
-c=list(map(int,input().split()))
-y=0
-m=0
-for i in c:
-    y+=(i//30+1)*10
-    m+=(i//60+1)*15
-if y>m:
-    print("M",m)
-elif y==m:
-    print("Y M",m)
-else:
-    print("Y",y)
+r1,average=map(int,input().split())
+result=average-r1
+print(average+result)
