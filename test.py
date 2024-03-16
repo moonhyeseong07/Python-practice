4e6429f1e3bfae8d4b9d6fdf4079b2a0d06e37b3
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-16 17:48:53 +0900

    codeup 1228

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-n=int(input())
-for i in range(n):
-    a=int(input())
-    if a%2==0:
-        print("even")
-    else:
-        print("odd")
+cm,kg=map(float,input().split())
+standard=(cm-100)*0.9
+biman=(kg-standard)*100/standard
+if biman<=10:
+    print("정상")
+elif biman>10 and biman<=20:
+    print("과체중")
+else:
+    print("비만")
