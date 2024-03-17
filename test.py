c68da41ea6fea0fa2bcf4beb141cf0ba0565af5c
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-17 23:01:20 +0900

    codeup 1533

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-cm,kg=map(float,input().split())
-standard=(cm-100)*0.9
-biman=(kg-standard)*100/standard
-if biman<=10:
-    print("정상")
-elif biman>10 and biman<=20:
-    print("과체중")
-else:
-    print("비만")
+def f():
+    return '%f' %3.14
+print(f())
