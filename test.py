b04530ae21da186f3ed3e69923704bfec83f7282
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-24 23:05:09 +0900

    baekjoon 2845

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-person,width=map(int,input().split())
-a,b,c,d,e=map(int,input().split())
-sum=person*width
-print(a-sum,b-sum,c-sum,d-sum,e-sum)
+l,p=map(int,input().split())
+numlist=list(map(int,input().split()))
+for num in numlist:
+    print(num-l*p,end=' ')
