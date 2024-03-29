4026d2547ca19e31ff6e4b14844f92688f2280d1
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-03-29 23:35:11 +0900

    baekjoon 1271

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-def f_ball(M,swaps):
-    ball_po=1
-    for swap in swaps:
-        x,y=swap
-        if ball_po==x:
-            ball_po=y
-        elif ball_po==y:
-            ball_po=x
-    return ball_po
-M=int(input())
-swaps=[list(map(int,input().split()))for _ in range(M)]
-print(f_ball(M,swaps)) 
+m,p=map(int,input().split())
+print(m//p)
+print(m%p)
