39708cc66656141cd1bde48f98e09f053f6522ec
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-04-01 23:08:33 +0900

    baekjoon 5532

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-res=0
-for i in range(4):
-    res+=int(input())
-print(res//60)
-print(res%60)
+l=int(input())
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+if a%c==0:
+    x=a//c
+else:
+    x=a//c+1
+if b%d==0:
+    y=b//d
+else:
+    y=b//d+1
+print(l-max(x,y))
