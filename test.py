a964a05df737de19abc00d9e4e1b8b0b9e187d07
moonhyeseong07 <moonhyeseong2007@gmail.com>
2024-04-03 22:30:53 +0900

    baekjoon 2446

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-print("|\\_/|")
-print("|q p|   /}")
-print('( 0 )"""\\')
-print('|"^"`    |')
-print("||_/=\\\\__|") 
+n=int(input())
+for i in range(n,1,-1):
+    print(" "*(n-i)+"*"*(2*i-1))
+for i in range(1,n+1):
+    print(" "*(n-i)+"*"*(2*i-1))
