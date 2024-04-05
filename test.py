3cbd145a83c9660d2d3ee54429efa63c5060952d
moonhyeseong07 <moonhyeseong2007@gmail.com>
2024-04-05 23:28:29 +0900

    baekjoon 3003

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-n=int(input())
-for i in range(n,1,-1):
-    print(" "*(n-i)+"*"*(2*i-1))
-for j in range(1,n+1):
-    print(" "*(n-j)+"*"*(2*j-1))
+p=[1,1,2,2,2,8]
+n=list(map(int,input().split()))
+for i in range(len(p)):
+    print(p[i]-n[i],end=' ')
