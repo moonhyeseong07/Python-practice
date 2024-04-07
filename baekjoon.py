e3892928faf91553cfce334f7aa4a08b554a401b
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-04-07 22:05:26 +0900

    baekjoon 2420

--- baekjoon.py
+++ baekjoon.py
@@ -1,7 +1,2 @@
-def darius(k,d,a):
-    if k+a<d or d==0:
-        return "hasu"
-    else:
-        return "gosu"
-k,d,a=map(int,input().split('/'))
-print(darius(k,d,a))
+a,b=map(int,input().split())
+print(abs(a-b))
