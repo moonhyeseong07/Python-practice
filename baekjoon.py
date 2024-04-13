18fb0f7e32d0395813a85d4048756115b27e19b7
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-04-13 22:28:36 +0900

    baekjoon 5543

--- baekjoon.py
+++ baekjoon.py
@@ -1,10 +1,7 @@
-print("     /~\\")
-print("    ( oo|")
-print("    _\\=/_")
-print("   /  _  \\")
-print("  //|/.\\|\\\\")
-print(" ||  \\ /  ||")
-print("============")
-print("|          |")
-print("|          |")
-print("|          |")
+a=[]
+b=[]
+for _ in range(3):
+    a.append(int(input()))
+for _ in range(2):
+    b.append(int(input()))
+print(min(a)+min(b)-50)
