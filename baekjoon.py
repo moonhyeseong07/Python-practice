cfb9b063f8e1046b75b2dbfc3f03e23f2c4f1742
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-04-14 23:40:35 +0900

    baekjoon 10101

--- baekjoon.py
+++ baekjoon.py
@@ -1,7 +1,12 @@
-a=[]
-b=[]
-for _ in range(3):
-    a.append(int(input()))
-for _ in range(2):
-    b.append(int(input()))
-print(min(a)+min(b)-50)
+a=int(input())
+b=int(input())
+c=int(input())
+if a+b+c==180:
+    if a==60 and b==60 and c==60:
+        print("Equilateral")
+    elif a==b or b==c or c==a:
+        print("Isosceles")
+    elif a!=b and b!=c:
+        print("Scalene")
+else:
+    print("Error")
