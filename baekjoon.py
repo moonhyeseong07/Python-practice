b9a6b1ff654518fa3f6ce745d0e4844ca0076ee1
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-06-01 22:54:18 +0900

    6/1

--- baekjoon.py
+++ baekjoon.py
@@ -1,10 +1,13 @@
 sum_price = int(input())
 n = int(input())
 result=0
+
 for i in range(1,n+1):
     price,count = map(int,input().split())
     result+=price*count
+    
 if result==sum_price:
     print("Yes")
+    
 else:
     print("No")
