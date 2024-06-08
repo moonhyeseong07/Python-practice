f9acf4c1a4608012d45cdd293b2c815f9e9abacb
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-06-08 23:47:55 +0900

    baekjoon 2490

--- baekjoon.py
+++ baekjoon.py
@@ -1,10 +1,12 @@
-a,b=map(int,input().split())
-array=[]
-for i in range(1,a+1):
-    if(a%i==0):
-        array.append(i)
-
-if(b<=len(array)):
-    print(array[b-1])
-else:
-    print(0)
+for _ in range(3):
+    n = sum(map(int, input().split()))
+    if n==4:
+        print("E")
+    elif n==3:
+        print("A")
+    elif n==2:
+        print("B")
+    elif n==1:
+        print("C")
+    else:
+        print("D")
