ec026ac75e7c14b4cb3d66f15bfc2eb982efc442
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2024-06-19 23:44:14 +0900

    dfs python

--- baekjoon.py
+++ baekjoon.py
@@ -1,13 +1,29 @@
-for _ in range(3):
+f = open("input.txt", "r")
+g = int(f.readline())
+graph = [[0 for _ in range(g)] for _ in range(g)]
+stack = []
+visited = [False for _ in range(g)]
+answer = []
 
-    n = sum(map(int, input().split()))
-    if n==4:
-        print("E")
-    elif n==3:
-        print("A")
-    elif n==2:
-        print("B")
-    elif n==1:
-        print("C")
-    else:
-        print("D")
+while True :
+    v = list(map(int, f.readline().split()))
+    if not v :
+        break
+    graph[v[0]][v[1]] = 1
+    graph[v[1]][v[0]] = 1
+
+def dfs(n) :
+    
+    stack.append(n)
+    
+    while len(answer) != g :
+        v = stack.pop()
+        visited[v] = True
+        answer.append(v + 1)
+        
+        for i in range(g-1, -1, -1) :
+            if graph[v][i] and not visited[i] :
+                stack.append(i)
+    print(*answer)
+
+dfs(0)
